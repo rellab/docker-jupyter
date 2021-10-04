67080d0d0b59f1a6f93669a182af3eafaf9f4a4f
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-10-04 05:00:54 +0000

    change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-echo "c.NotebookApp.token = '$JUPYTER_PASSWORD'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
-start-notebook.sh
+groupadd -f -g $JUPYTER_GID $JUPYTER_GROUP || exit 1
+if [ $JUPYTER_PASSWORD = "no" ]; then
+  useradd -d $JUPYTER_HOME -u $JUPYTER_UID -g $JUPYTER_GID -s /bin/bash $JUPYTER_USER || exit 1
+else
+  useradd -d $JUPYTER_HOME -u $JUPYTER_UID -g $JUPYTER_GID -s /bin/bash -p `echo "$JUPYTER_PASSWORD" | mkpasswd -s -m sha-512` $JUPYTER_USER || exit 1
+fi
+
+if [ $JUPYTER_GRANT_SUDO = "yes" ]; then
+  echo "$JUPYTER_USER ALL=(ALL) ALL" >> /etc/sudoers.d/$JUPYTER_USER
+elif [ $JUPYTER_GRANT_SUDO = "nopass" ]; then
+  echo "$JUPYTER_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$JUPYTER_USER
+fi
+
+mkdir -p $JUPYTER_HOME
+chown $JUPYTER_USER:$JUPYTER_GROUP $JUPYTER_HOME
+su - $JUPYTER_USER -c "cp -n -r --preserve=mode /etc/skel/. $JUPYTER_HOME"
+
+su - $JUPYTER_USER -c "echo \"c.NotebookApp.token = '$JUPYTER_PASSWORD'\" >> $JUPYTER_HOME/.jupyter/jupyter_notebook_config.py"
+cd $JUPYTER_HOME
+su - $JUPYTER_USER -c "start-notebook.sh"
+
