950439dfc06edb733976ba501485395c0069a361
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-10-05 01:40:43 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "$JUPYTER_USER:$JUPYTER_PASSWORD" | chpasswd
 
 if [ $JUPYTER_GRANT_SUDO = "yes" ]; then
   echo "$JUPYTER_USER ALL=(ALL) ALL" >> /etc/sudoers.d/$JUPYTER_USER
-elif [ $JUPYTER_USER_GRANT_SUDO = "nopass" ]; then
+elif [ $JUPYTER_GRANT_SUDO = "nopass" ]; then
   echo "$JUPYTER_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$JUPYTER_USER
 fi
 
