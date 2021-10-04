a38ee52671a97c416f5e4f9523d64832eba0db86
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-10-05 02:08:07 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-echo "$JUPYTER_USER:$JUPYTER_PASSWORD" | chpasswd
-
-if [ $JUPYTER_GRANT_SUDO = "yes" ]; then
-  echo "$JUPYTER_USER ALL=(ALL) ALL" >> /etc/sudoers.d/$JUPYTER_USER
-elif [ $JUPYTER_GRANT_SUDO = "nopass" ]; then
-  echo "$JUPYTER_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$JUPYTER_USER
-fi
-
-su - $JUPYTER_USER -c "echo \"c.NotebookApp.token = '$JUPYTER_PASSWORD'\" >> /home/jovyan/.jupyter/jupyter_notebook_config.py"
-su - $JUPYTER_USER -c "start-notebook.sh"
+echo \"c.NotebookApp.token = '$JUPYTER_PASSWORD'\" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
+start-notebook.sh
