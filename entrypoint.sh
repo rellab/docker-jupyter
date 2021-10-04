b4667b9283fa9e70bd3e5f75a51fa6d9eb969dff
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-10-05 02:11:07 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo \"c.NotebookApp.token = '$JUPYTER_PASSWORD'\" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.token = '$JUPYTER_PASSWORD'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
 start-notebook.sh
